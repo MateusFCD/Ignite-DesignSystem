9a9bd668d454e668c262feb41b29fc84b9316196
MateusFCD <mateusdrumond.dev@gmail.com>
2023-08-15 18:49:04 -0300

    testing deploy

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -2,7 +2,7 @@ import { StoryObj, Meta } from "@storybook/react";
 import { Avatar, AvatarProps } from "@ignite-ui/react";
 
 export default {
-  title: "Data display/Avatar",
+  title: "Data-display/Avatar",
   component: Avatar,
   args: {
     src: "https://github.com/MateusFCD.png",
